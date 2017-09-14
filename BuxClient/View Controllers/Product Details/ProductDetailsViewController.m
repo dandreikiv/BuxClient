1e5af298a7298a947b3f250840bf2d706e007b97
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-14 21:52:41 +0200

    Added Product Details

---
+++ BuxClient/View Controllers/Product Details/ProductDetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ProductDetailsViewController.m
+//  BuxClient
+//
+//  Created by dmytro.andreikiv@philips.com on 14/09/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+#import "ProductDetailsViewController.h"
+
+@interface ProductDetailsViewController ()
+
+@end
+
+@implementation ProductDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
