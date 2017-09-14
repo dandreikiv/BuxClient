3ced0d24096ffed2f2186725324a7a8befdccf8c
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-14 20:18:57 +0200

    Added output method to update list for received products.

--- BuxClient/Data Coordinator/DataCoordinatorOutput.h
+++ BuxClient/Data Coordinator/DataCoordinatorOutput.h
@@ -1,12 +1,15 @@
 //
-//  DataCoordinatorDelegate.h
+//  DataCoordinatorOutput.h
 //  BuxClient
 //
 //  Created by dmytro.andreikiv@philips.com on 14/09/2017.
 //  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
 //
 
-@protocol DataCoordinatorDelegate <NSObject>
+@class ProductListViewModel;
 
+@protocol DataCoordinatorOutput <NSObject>
+
+- (void)updateListWithProducts:(NSArray <ProductListViewModel *> *)products;
 
 @end
