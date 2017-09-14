43fadc3ff582797e17128f2ac2594367cc0fc81a
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-14 11:46:10 +0200

    Added Data Cordinator group

---
+++ BuxClient/Data Coordinator/DataCoordinator.h
@@ -0,0 +1,13 @@
+//
+//  DataCoordinator.h
+//  BuxClient
+//
+//  Created by dmytro.andreikiv@philips.com on 14/09/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataCoordinator : NSObject
+
+@end
---
+++ BuxClient/Data Coordinator/DataCoordinator.m
@@ -0,0 +1,13 @@
+//
+//  DataCoordinator.m
+//  BuxClient
+//
+//  Created by dmytro.andreikiv@philips.com on 14/09/2017.
+//  Copyright © 2017 Dmytro Andreikiv. All rights reserved.
+//
+
+#import "DataCoordinator.h"
+
+@implementation DataCoordinator
+
+@end
