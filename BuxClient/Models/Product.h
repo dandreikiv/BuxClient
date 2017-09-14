4135b89de6c63bda8d8adc7ecc122902c2d8fe5d
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-14 07:30:01 +0200

    Added additional fields

--- BuxClient/Models/Product.h
+++ BuxClient/Models/Product.h
@@ -8,6 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+@class Price;
+
 @interface Product : NSObject
+
+@property (nonatomic, copy, readonly) NSString *symbol;
+@property (nonatomic, copy, readonly) NSString *displayName;
+@property (nonatomic, copy, readonly) NSString *productId;
+@property (nonatomic, copy, readonly) NSString *category;
+@property (nonatomic, copy, readonly) NSString *productDescription;
+@property (nonatomic, copy, readonly) NSString *quoteCurrency;
+@property (nonatomic, strong, readonly) Price *currentPrice;
+@property (nonatomic, strong, readonly) Price *closingPrice;
+
+- (instancetype)initWithProductDictionary:(NSDictionary *)dictionary;
+
+- (void)updateCurrentPriceWithAmount:(NSNumber *)amount;
 
 @end
--- BuxClient/Models/Product.m
+++ BuxClient/Models/Product.m
@@ -7,7 +7,61 @@
 //
 
 #import "Product.h"
+#import "Price.h"
+
+static NSString *const kSymbol = @"symbol";
+static NSString *const kDisplayName = @"displayName";
+static NSString *const kDescription = @"description";
+static NSString *const kCurrentPrice = @"currentPrice";
+static NSString *const kClosingPrice = @"closingPrice";
+static NSString *const kProductId = @"securityId";
+static NSString *const kCategory = @"category";
+static NSString *const kQuoteCurrency = @"quoteCurrency";
+
+@interface Product()
+
+@property (nonatomic, copy) NSString *symbol;
+@property (nonatomic, copy) NSString *displayName;
+@property (nonatomic, copy) NSString *productId;
+@property (nonatomic, copy) NSString *category;
+@property (nonatomic, copy) NSString *productDescription;
+@property (nonatomic, copy) NSString *quoteCurrency;
+@property (nonatomic, strong) Price *currentPrice;
+@property (nonatomic, strong) Price *closingPrice;
+
+@end
 
 @implementation Product
+
+- (instancetype)initWithProductDictionary:(NSDictionary *)dictionary {
+	self = [super init];
+	if (self) {
+		self.symbol = dictionary[kSymbol];
+		self.displayName = dictionary[kDisplayName];
+		self.productId = dictionary[kProductId];
+		self.category = dictionary[kCategory];
+		self.productDescription = dictionary[kDescription];
+		
+		self.currentPrice = [[Price alloc] initWithPriceDictionary:dictionary[kCurrentPrice]];
+		self.closingPrice = [[Price alloc] initWithPriceDictionary:dictionary[kClosingPrice]];
+	}
+	return self;
+}
+
+- (NSString *)description {
+	return [NSString stringWithFormat:@"%@: %@", self.displayName, self.productId];
+}
+
+- (void)updateCurrentPriceWithAmount:(NSNumber *)amount {
+	[self.currentPrice updateAmount:amount];
+}
+
+- (BOOL)isEqual:(Product *)object {
+	if (self == object) {
+		return YES;
+	}
+	
+	return [self.productId isEqualToString:object.productId];
+}
 
 @end
