5ab1abeb976c9201501d2d4dc4a53cf805188b80
Dmytro Andreikiv <dmytro.andreikiv@philips.com>
2017-09-20 20:17:37 +0200

    Added comments.

--- BuxClient/Data Coordinator/Data Storage/DataStorageProtocol.h
+++ BuxClient/Data Coordinator/Data Storage/DataStorageProtocol.h
@@ -10,13 +10,52 @@
 
 @protocol DataStorageProtocol <NSObject>
 
+/**
+ Returns total number of products in data storage.
+ */
 @property (nonatomic, assign, readonly) NSUInteger count;
 
-- (void)storeProduct:(Product *)product;
-- (void)storeProducts:(NSArray <Product *> *)products;
-- (void)update:(Product *)product withId:(NSString *)productId;
+/**
+ Stores given product to data storage.
 
-- (Product *)productWithId:(NSString *)productId;
-- (NSArray <Product *> *)allProducts;
+ @param product a product to store.
+ */
+- (void)storeProduct:(nonnull Product *)product;
+
+
+/**
+ Stores a list of prouducts to data storage.
+
+ @param products a list of products.
+ */
+- (void)storeProducts:(nonnull NSArray <Product *> *)products;
+
+
+/**
+ Updates product in data storage which identifier is equal to given productId with info
+ from given product.
+
+ @param product product which has new data.
+ @param productId identifier of a product which should be updated.
+ */
+- (void)update:(nonnull Product *)product
+		withId:(nonnull NSString *)productId;
+
+
+/**
+ Returns a product with given prouductId.
+
+ @param productId identifier of a product to return.
+ @return Product or nil, in case product with given id was not fouund.
+ */
+- (nullable Product *)productWithId:(nonnull NSString *)productId;
+
+
+/**
+ Returns list of all stored products in datastorage.
+
+ @return a list of products.
+ */
+- (nullable NSArray <Product *> *)allProducts;
 
 @end
